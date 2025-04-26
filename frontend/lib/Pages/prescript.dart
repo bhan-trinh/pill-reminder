b5ef006b017631bdacdc50082558103a7882b958
fweamy <baohanmi@gmail.com>
2025-04-26 18:23:12 -0400

    Change file path in import

---
+++ frontend/lib/Pages/prescript.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/Widgets/med_card.dart';
+import '../Themes/AppColors.dart';
+
+class PrescriptPage extends StatelessWidget {
+  const PrescriptPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: AppColors.primaryBackground,
+      ),
+      body: ListView.builder(
+        
+        itemCount: 2,
+        itemBuilder: (BuildContext context, int index) {
+          return MedCard();
+          },
+      ),
+      
+     );
+  }
+}
