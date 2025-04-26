e89c946d88ab5cc2c5afe53cf2fa8ecf64c7183f
fweamy <baohanmi@gmail.com>
2025-04-26 16:34:33 -0400

    Change Navigation colors to match design

--- frontend/lib/navigation_menu.dart
+++ frontend/lib/navigation_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'home.dart';
+import 'Themes/AppColors.dart';
 
 class NavigationMenu extends StatelessWidget {
   const NavigationMenu({super.key});
@@ -12,12 +13,14 @@ class NavigationMenu extends StatelessWidget {
     return Scaffold(
       bottomNavigationBar: Obx (
         () => NavigationBar(
+        backgroundColor: AppColors.secondaryBackground,
+        indicatorColor: AppColors.accent1,
         selectedIndex: controller.selectedIndex.value,
         onDestinationSelected: (index) => controller.selectedIndex.value = index,
         destinations: [
-          NavigationDestination(icon: const Icon(Icons.home), label: "Home"),
-          NavigationDestination(icon: const Icon(Icons.book), label: "Prescriptions"),
-          NavigationDestination(icon: const Icon(Icons.settings), label: "Settings"),
+          NavigationDestination(icon: const Icon(Icons.home, color: Colors.black), label: "Home"),
+          NavigationDestination(icon: const Icon(Icons.book, color: Colors.black), label: "Prescriptions"),
+          NavigationDestination(icon: const Icon(Icons.settings, color: Colors.black), label: "Settings"),
         ],
         )
         ),
