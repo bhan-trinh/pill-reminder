fc68e03af96bc0769a68da512be9213018344cac
nt636@tux.cci.drexel.edu <khanhnguyentrinhthi@gmail.com>
2025-04-27 08:58:07 -0400

    Fix serial server

--- server.py
+++ server.py
@@ -3,13 +3,15 @@ import re
 from flask_restful import Resource, Api
 from google import genai
 import os
+from dotenv import load_dotenv
+import threading
+
 import time
 import datetime
-import threading
 from backend.hardware.python import serialcontrol
-from dotenv import load_dotenv
 import serial
 from serial.tools import list_ports
+import checkport
 
 load_dotenv()
 
@@ -19,30 +21,8 @@ client = genai.Client(api_key=api_key)
 app = flask.Flask(__name__)
 api = Api(app)
 
-pill_drop_status = None
-connected_port = None
-
-def find_connected_port():
-    global connected_port
-    ports = list_ports.comports()
-    for port in ports:
-        if 'usbmodem' in port.device:
-            connected_port = port.device
-            print(f"Found connected port: {connected_port}")
-            return connected_port
-    return None
-
-def check_serial_connection():
-    if connected_port:
-        try:
-            ser = serial.Serial(connected_port)
-            if ser.is_open:
-                return True
-        except serial.SerialException:
-            return False
-    return False
-
 def ocr(filename):
+    
     file = client.files.upload(file=filename)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
@@ -55,34 +35,12 @@ def ocr(filename):
 
     return {"medication_name":medication_name, "dosage":dosage, "instructions":instructions, "times":times}
 
-def runSerial():
-    global pill_drop_status
-    scheduled_hour = 7 
-    last_drop_date = None
-    while True:
-        if not connected_port:
-            find_connected_port()
 
-        current_date = datetime.datetime.now().date
-        serial_status = "connected" if check_serial_connection() else "thread running, not connected"
-        print(f"Serial Status: {serial_status}")
-        
-        if serial_status == "connected" and last_drop_date != current_date:
-            print(f"Serial port connected ({connected_port}), attempting to drop pills...")
-            serialcontrol.dropPills(
-                dose=2,
-                time_to_drop=scheduled_hour,
-                serial_port=connected_port
-            )
-            print("Pills dropped successfully.")
-            pill_drop_status = "Pills dropped successfully."
-            last_drop_date == current_date
-        time.sleep(30)
 
 class Label(Resource):
     def post(self):
         file = flask.request.files['file']
-        temp_path = f"/tmp/{file.filename}"
+        temp_path = f"/tmp/{file.filename}"  # save inside /tmp, safe on Render
         file.save(temp_path)
 
         output = ocr(temp_path)
@@ -100,13 +58,9 @@ api.add_resource(Label, "/")
 
 @app.route("/", methods=["GET"])
 def home():
-    serial_status = "connected" if check_serial_connection() else "thread running, not connected"
-    return {
-        "message": "Server is running!",
-        "serial_status": serial_status,
-        "pill_drop_status": pill_drop_status 
-    }, 200
+    return {"message": "Server is running!"},200
+
 
 if __name__ == "__main__":
-    threading.Thread(target=runSerial).start()
+    threading.Thread(target=checkport.runSerial).start()
     app.run(debug=True, host="0.0.0.0", port=10000)
